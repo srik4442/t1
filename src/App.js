a877a9ca4d7744ab96f59295123ee07559ee45aa
Srikara Thikkireddy <130203062+srik4442@users.noreply.github.com>
2023-10-14 20:34:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,10 +11,3 @@ function App() {
 }
 
 export default App;
-echo "# t1" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/srik4442/t1.git
-git push -u origin main
